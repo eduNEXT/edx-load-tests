cfd6faa78610afd5adafa57ca6417fa1a0c7ae38
Eric Herrera <eric.herrera@edunext.co>
2020-03-20 17:58:56 -0500

    Add user views tests.

---
+++ loadtests/edunext/lms/user_views.py
@@ -0,0 +1,24 @@
+import logging
+
+from locust import task
+from .base import EdunextLmsTasks
+
+
+class StudentViewsTasks(EdunextLmsTasks):
+    """
+    Models traffic for endpoints in student.views.
+    """
+
+    def _request(self, method, path, *args, **kwargs):
+        """
+        Overriden method to handle paths that are not realted to the courses.
+        """
+        return getattr(self.client, method)(path, *args, **kwargs)
+
+    @task(1)
+    def dashboard(self):
+        """
+        Request the dashboard.
+        """
+        path = 'dashboard'
+        self.get(path, name='dashboard')
